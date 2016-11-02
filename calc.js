fa27c12e81d106bf1cb468958cfbefd2e793e12c
Stefanie Gonzalez <sgonzalez.sh@mysummitps.org>
2016-11-02 23:59:31 +0000

    update

--- calc.js
+++ calc.js
@@ -1,1 +1,5 @@
-var num1Input = document.getElementById("num1input");
+var num1Input = document.getElementById("numIinput");
+var operationInput = document.getElementById("operationnput");
+var num2Input = document.getElementById("num2Input");
+var calculatenowButton = document.getElementById("calculatenowButton");
+var resetButton = document.getElementById("resetButton");
