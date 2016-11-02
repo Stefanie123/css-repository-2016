8deb0758864b9c7b041f20f873d9059551c77d4a
Stefanie Gonzalez <sgonzalez.sh@mysummitps.org>
2016-11-02 22:08:21 +0000

    update

---
+++ calc.js
@@ -0,0 +1,7 @@
+<p id="demo">Click the button to change the text in this paragraph.</p>
+<button onclick="myFunction()">Try it</button>
+
+var num1 = 
+var num2 = 
+
+if(){
--- calculator.js
+++
@@ -1,3 +0,0 @@
-<p id="demo">Click the button to reveal the answer.</p>
-
-<button onclick="myFunction()">Calculate Now</button>
