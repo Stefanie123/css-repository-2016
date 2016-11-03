6ccc2ff562bdfa7e614e52de180c41ac71432c41
Stefanie Gonzalez <sgonzalez.sh@mysummitps.org>
2016-11-03 21:48:40 +0000

    update

--- calc.js
+++ calc.js
@@ -1,5 +1,38 @@
+//
 var num1Input = document.getElementById("numIinput");
 var operationInput = document.getElementById("operationnput");
 var num2Input = document.getElementById("num2Input");
 var calculatenowButton = document.getElementById("calculatenowButton");
 var resetButton = document.getElementById("resetButton");
+var answer = document.getElementById("answer");
+
+calculatenowButton.addEventListener("click", function (){
+    var num1 = varnum1Input.value;
+    var num2 = varnum2Input.value;
+    var answer = varanswer.value;
+})
+
+if (operation.value --- "+"){
+
+}
+else(){
+
+}
+if (operation.value --- "-"){
+
+}
+else(){
+
+}
+if (operation.value --- "*"){
+
+}
+else(){
+
+}
+if (operation.value --- "/"){
+
+}
+else(){
+
+}
