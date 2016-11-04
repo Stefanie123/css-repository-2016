8183a6b13c58af257c271860a42777bc0e03807c
Stefanie Gonzalez <sgonzalez.sh@mysummitps.org>
2016-11-04 01:02:04 +0000

    update

--- calc.js
+++ calc.js
@@ -3,43 +3,43 @@ var num1Input = document.getElementById("numIinput");
 var operationInput = document.getElementById("operationnput");
 var num2Input = document.getElementById("num2Input");
 var calculatenowButton = document.getElementById("calculatenowButton");
-var resetButton = document.getElementById("resetButton");
 var answer = document.getElementById("answer");
 
-calculatenowButton.addEventListener("click", function (){
+calculatenowButton.addEventListener("click", function() {
     var num1 = varnum1Input.value;
     var num2 = varnum2Input.value;
     var answer = varanswer.value;
+
+    if (operation.value-- - "+") {
+        answer.innerHTML = (num1) + (num2)
+    }
+
+    else if (operation.value-- - "+") {
+        answer.innerHTML = (num1) + (num2)
+    }
+
+    if (operation.value-- - "-") {
+        answer.innerHTML = (num1) - (num2)
+    }
+
+    else if (operation.value-- - "-") {
+        answer.innerHTML = (num1) - (num2)
+    }
+
+    if (operation.value-- - "*") {
+        answer.innerHTML = (num1) * (num2)
+    }
+
+    else if (operation.value-- - "*") {
+        answer.innerHTML = (num1) * (num2)
+    }
+
+    if (operation.value-- - "/") {
+        answer.innerHTML =  (num1) / (num2)
+    }
+
+    else if (operation.value-- - "/") {
+        answer.innerHTML =  (num1) / (num2)
+    }
+
 })
-
-if (operation.value ---"+"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-else if (operation.value ---"+"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-if (operation.value ---"-"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-else if (operation.value ---"-"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-if (operation.value ---"*"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-else if (operation.value ---"*"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-if (operation.value ---"/"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
-
-else if (operation.value ---"/"){
-answer.innerHTML = Number 1 (num1)+ Number 2 (num2)
-}
