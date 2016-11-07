d81956b026085c81abcb858233396776591732ff
Stefanie Gonzalez <sgonzalez.sh@mysummitps.org>
2016-11-07 22:14:13 +0000

    update

---
+++ todo.js
@@ -0,0 +1,10 @@
+//Connects todo.html to todo.js in order to work.
+var textinput = document.getElementById("textinput");
+var stylingdropdown= document.getElementById("stylingdropdown");
+var typebutton= document.getElementById("typebutton");
+var tasksdiv= document.getElementById("tasksdiv");
+
+typebutton.addEventListener("click", function (){
+  var task = "<P>" + textinput.value + "<P>"
+  div.innerHTML
+})
